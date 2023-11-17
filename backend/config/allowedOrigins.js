77edcb1057686814ae97bef2da13eb6c54ac0004
Bon Lacorte <bonbonlacorte123@gmail.com>
2023-11-17 09:24:06 +0800

    7th add

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
     'http://localhost:3500',
     'http://localhost:3501',
     'http://localhost:3502',
-    'https://pup-find-server.vercel.app/',
+    'https://pupfind.vercel.app',
     'https://pupfind.onrender.com'
 ]
 
